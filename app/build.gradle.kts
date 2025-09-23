7a01b8b481bc05bdfbb76b7b91e32b23f50b9a95
motoki <benmujiantai@gmail.com>
2025-09-23 20:58:41 +0900

    versionCodeを2に変更

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,8 +29,8 @@ android {
         applicationId = "com.k.simplememo"
         minSdk = 24
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
